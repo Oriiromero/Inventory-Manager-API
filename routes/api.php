183c5c54b98dc6e7b55171e0b420208488c5766a
oriana <oriana.nahir.r@gmail.com>
2024-06-13 14:47:10 +0200

    started routing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuditLogController;
+use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PackageMoveController;
+use App\Http\Controllers\SupermarketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'test'], function() {
+    Route::apiResource('packages', PackageController::class);
+    Route::apiResource('packages-moves', PackageMoveController::class);
+    Route::apiResource('supermarkets', SupermarketController::class);
+    Route::apiResource('audit-logs', AuditLogController::class);
 });
